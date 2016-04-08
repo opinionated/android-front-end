4cf91041fea394d8eb39c8b4ca46e2a2d8978471
matthewmawby <themesta@gmail.com>
2016-04-08 13:23:25 -0400

    image sizes change dynamically based on screen size - prettification has happened

--- Opinionated/app/src/main/java/com/opinionated/opinionated/main_page.java
+++ Opinionated/app/src/main/java/com/opinionated/opinionated/main_page.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.Point;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.Xml;
+import android.view.Display;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -97,11 +99,18 @@ public class main_page extends AppCompatActivity {
                         art_layout.addView(button);
 
 
-                        //Load an image from it's URL and place below the button just instantiated
+                        //get the size of the display to make the image fit the display
+                        Display display = getWindowManager().getDefaultDisplay();
+                        Point size = new Point();
+                        display.getSize(size);
+                        int width = size.x;
+                        int height=size.y;
 
+
+                        //Load an image from it's URL and place below the button just instantiated
                         ImageView image = (ImageView) new ImageView(this);
                         String url_string=article.getString("image");
-                        Picasso.with(this).load(url_string).resize(800,300).centerCrop().into(image);
+                        Picasso.with(this).load(url_string).resize(width,height/4).centerCrop().into(image);
                         art_layout.addView(image);
 
                         linearLayout.addView(art_layout);
