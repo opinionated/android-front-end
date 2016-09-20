802a8757cbc7ac77e51b18780563016e4c7b8ed9
MatthewMawby <themesta@gmail.com>
2016-09-20 17:34:03 -0400

    minor changes

--- Opinionated/app/src/main/java/com/opinionated/opinionated/ArticleViewer.java
+++ Opinionated/app/src/main/java/com/opinionated/opinionated/ArticleViewer.java
@@ -1,10 +1,13 @@
 package com.opinionated.opinionated;
 
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Display;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
